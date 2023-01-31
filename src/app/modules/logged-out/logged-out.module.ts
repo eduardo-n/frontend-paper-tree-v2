406ade771c30563f2eaf98b44a2bc2f5525304bc
Eduardo Nunes <edununes726@gmail.com>
2023-01-31 12:30:49 -0300

    Criacao dos dois modulos principais

---
+++ src/app/modules/logged-out/logged-out.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoggedOutRoutingModule } from './logged-out-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoggedOutRoutingModule
+  ]
+})
+export class LoggedOutModule { }
---
+++ src/app/modules/logged/logged.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoggedRoutingModule } from './logged-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    LoggedRoutingModule
+  ]
+})
+export class LoggedModule { }
