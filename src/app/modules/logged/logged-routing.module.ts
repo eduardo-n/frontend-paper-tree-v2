0374c345c7147ccc6cffd19157ee99afed83f503
Eduardo Nunes <edununes726@gmail.com>
2023-02-17 18:15:49 -0300

    Removendo Modulos

--- src/app/modules/logged/logged-routing.module.ts
+++ src/app/modules/logged/logged-routing.module.ts
@@ -1,18 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FeedComponent } from './modules/home/pages/feed/feed.component';
 
-const routes: Routes = [
-  {
-    path:'',
-    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
-    //canActivate: []
-  },
-  {
-    path:'**',
-    redirectTo: ''
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
