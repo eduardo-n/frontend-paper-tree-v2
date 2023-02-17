e76140a985aac6f9a5226bb1d00fc722f75c80cd
Eduardo Nunes <edununes726@gmail.com>
2023-02-17 17:39:37 -0300

    Removendo import desnecessario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
-import { LoggedOutModule } from './modules/logged-out/logged-out.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +15,6 @@ import { LoggedOutModule } from './modules/logged-out/logged-out.module';
     BrowserAnimationsModule,
     RouterModule,
     AppRoutingModule,
-    LoggedOutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
