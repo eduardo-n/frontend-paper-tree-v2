4b6c109ceeaef5070aa84d279eaef0c20eac8cf6
Eduardo Nunes <edununes726@gmail.com>
2023-02-20 20:11:42 -0300

    Rotas, componetes de header e feed

--- src/app/modules/logged/logged.module.ts
+++ src/app/modules/logged/logged.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { LoggedRoutingModule } from './logged-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderBarComponent,
+    children: [
+      {
+        path:'',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+        //canActivate: []
+      },
+      {
+        path:'**',
+        redirectTo: ''
+      },
+    ]
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderBarComponent],
   imports: [
-    CommonModule,
-    LoggedRoutingModule
-  ]
+    RouterModule.forChild(routes),
+    MaterialModule
+  ],
+  exports: [RouterModule]
 })
 export class LoggedModule { }
