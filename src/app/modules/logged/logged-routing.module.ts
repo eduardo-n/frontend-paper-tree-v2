7e1077ebfc8f04d2039a8e29141527a6a64e9a9d
Eduardo Nunes <edununes726@gmail.com>
2023-02-20 20:18:12 -0300

    Correcao nas rotas

--- src/app/modules/logged/logged-routing.module.ts
+++ src/app/modules/logged/logged-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HeaderBarComponent,
+    children: [
+      {
+        path:'',
+        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+        //canActivate: []
+      },
+      {
+        path:'**',
+        redirectTo: ''
+      },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/logged/logged.module.ts
+++ src/app/modules/logged/logged.module.ts
@@ -1,32 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { LoggedRoutingModule } from './logged-routing.module';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
-
-const routes: Routes = [
-  {
-    path: '',
-    component: HeaderBarComponent,
-    children: [
-      {
-        path:'',
-        loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
-        //canActivate: []
-      },
-      {
-        path:'**',
-        redirectTo: ''
-      },
-    ]
-  }
-];
+import { LoggedRoutingModule } from './logged-routing.module';
 
 @NgModule({
   declarations: [HeaderBarComponent],
   imports: [
-    RouterModule.forChild(routes),
+    LoggedRoutingModule,
     MaterialModule
   ],
   exports: [RouterModule]
