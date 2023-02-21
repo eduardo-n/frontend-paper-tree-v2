d5b4ec6b68085831638b16fac9c22982b3e821a0
Eduardo Nunes <edununes726@gmail.com>
2023-02-20 23:14:26 -0300

    Ajuste padding

--- src/app/modules/logged/modules/home/home.module.ts
+++ src/app/modules/logged/modules/home/home.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { FeedComponent } from './feed/feed.component';
 import { EmptyContentCardComponent } from './components/empty-content-card/empty-content-card.component';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 
 @NgModule({
   declarations: [
     FeedComponent,
-    EmptyContentCardComponent
+    EmptyContentCardComponent,
+    PostCardComponent
   ],
   imports: [
     CommonModule,
