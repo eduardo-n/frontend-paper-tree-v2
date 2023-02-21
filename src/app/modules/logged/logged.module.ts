0ecb7380e364b41282c0674e1bd37a7f9e8a3493
Eduardo Nunes <edununes726@gmail.com>
2023-02-20 23:31:52 -0300

    Mudando componente de post-card de pasta

--- src/app/modules/logged/logged.module.ts
+++ src/app/modules/logged/logged.module.ts
@@ -3,9 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderBarComponent } from './components/header-bar/header-bar.component';
 import { MaterialModule } from 'src/app/shared/modules/material/material.module';
 import { LoggedRoutingModule } from './logged-routing.module';
+import { PostCardComponent } from './components/post-card/post-card.component';
 
 @NgModule({
-  declarations: [HeaderBarComponent],
+  declarations: [HeaderBarComponent, PostCardComponent],
   imports: [
     LoggedRoutingModule,
     MaterialModule
--- src/app/modules/logged/modules/home/home.module.ts
+++ src/app/modules/logged/modules/home/home.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { FeedComponent } from './feed/feed.component';
 import { EmptyContentCardComponent } from './components/empty-content-card/empty-content-card.component';
-import { PostCardComponent } from './components/post-card/post-card.component';
 
 
 @NgModule({
   declarations: [
     FeedComponent,
-    EmptyContentCardComponent,
-    PostCardComponent
+    EmptyContentCardComponent
   ],
   imports: [
     CommonModule,
