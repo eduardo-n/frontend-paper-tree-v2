ef1cabeeced7e02aa0e391070ae727f411abe5c7
Eduardo Nunes <edununes726@gmail.com>
2023-02-20 23:01:17 -0300

    Card para informar que nao ha conteudo

--- src/app/modules/logged/modules/home/home.module.ts
+++ src/app/modules/logged/modules/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { FeedComponent } from './feed/feed.component';
+import { EmptyContentCardComponent } from './components/empty-content-card/empty-content-card.component';
 
 
 @NgModule({
   declarations: [
-    FeedComponent
+    FeedComponent,
+    EmptyContentCardComponent
   ],
   imports: [
     CommonModule,
